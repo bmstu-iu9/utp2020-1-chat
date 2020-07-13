6643182fbb311d371a9c4d6072e242d1d7e6f627
Klaus1901 <zholtikov.mixail@mail.ru>
2020-07-13 14:38:59 +0300

    Добавил пользователю имя

--- models/user.js
+++ models/user.js
@@ -1,15 +1,16 @@
 const fs = require('fs')
 
 class User{
-    constructor(username, password) {
+    constructor(username, login, password) {
         this.username = username;
+        this.login = login;
         this.password = password;
     }
     check(){
         let data = fs.readFileSync("../data/users.json", 'utf-8');
         let users = JSON.parse(data);
         for(let i=0;i<users.length;i++){
-            if (this.username === users[i].username) return true;
+            if (this.login === users[i].login) return true;
         }
         return false
     }
@@ -23,4 +24,5 @@ class User{
 }
 
 module.exports = User;
+
 

78ee20e3c59d05c7c81e29e4f1a2b43d26d05b95
Klaus1901 <zholtikov.mixail@mail.ru>
2020-07-17 13:31:43 +0300

    Исправил оформление
    # Пожалуйста, введите сообщение коммита, для объяснения, зачем нужно
    # это слияние, особенно, если это слияние обновленной вышестоящей
    # ветки в тематическую ветку.
    #
    # Строки, начинающиеся с «#» будут проигнорированы, а пустое
    # сообщение отменяет процесс коммита.

--- models/user.js
+++ models/user.js
@@ -4,8 +4,8 @@ let users = [];
 function joinUser(socketId, userName, roomName) {
     const user = {
         socketID :  socketId,
-        username : userName,
-        roomname : roomName
+        userName : userName,
+        roomName : roomName
     }
     users.push(user)
     return user;

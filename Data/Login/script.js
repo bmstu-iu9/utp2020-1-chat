ffdf8365a534e7a5bba645ce24a7d60c437f178e
Nikita Rezepin <rezepinn@yandex.ru>
2020-07-22 01:23:04 +0300

    Решение вопроса #17
    Добавлен блок-переключатель отображения символов пароля при вводе
    Добавлены соответствующие текстуры, стили и скрипт переключения

--- Data/Login/script.js
+++ Data/Login/script.js
@@ -7,3 +7,15 @@ function registration() {
 function signin() {
     alert("sign in prover0chka");
 }
+
+function togglePassword() {
+    let password = document.getElementById("password");
+    let eye = document.getElementById("password-hider");
+    if (password.type === "password") {
+        password.type = "text";
+        eye.className = "eye-opened";
+    } else {
+        password.type = "password";
+        eye.className = "eye-closed";
+    }
+}

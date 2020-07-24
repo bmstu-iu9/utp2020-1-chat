f6fc6c3965d9f3bc42292f22ba5ea73f3e7cf2a0
Nikita Rezepin <rezepinn@yandex.ru>
2020-07-24 20:14:15 +0300

    Реструктурирование файлов проекта

---
+++ Data/Login/scripts/eye-opener.js
@@ -0,0 +1,13 @@
+"use strict";
+
+function togglePassword() {
+    let password = document.getElementById("password");
+    let eye = document.getElementById("password-hider");
+    if (password.type === "password") {
+        password.type = "text";
+        eye.className = "eye-opened";
+    } else {
+        password.type = "password";
+        eye.className = "eye-closed";
+    }
+}

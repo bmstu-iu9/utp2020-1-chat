a24e3291393d4f35d3145b7be361ece00152c8c3
Klaus1901 <zholtikov.mixail@mail.ru>
2020-08-01 00:51:15 +0300

    Упростил код в registration.js
    # Пожалуйста, введите сообщение коммита, для объяснения, зачем нужно
    # это слияние, особенно, если это слияние обновленной вышестоящей
    # ветки в тематическую ветку.
    #
    # Строки, начинающиеся с «#» будут проигнорированы, а пустое
    # сообщение отменяет процесс коммита.

--- routes/registration.js
+++ routes/registration.js
@@ -19,39 +19,36 @@ router.post('/', (req, res) =>{
     const {name, login, password} = req.body;
     console.log(' Name ' + name+ ' login :' + login+ ' pass:' + password);
     if(!name || !login || !password) {
-        res.render("registration.ejs");
+        return res.render("registration.ejs");
     }
-    else {
-        if (password.length < 6) {
-            res.render("registration.ejs");
-        }
-        else{
-            User.findOne({login : login}).exec((err,user)=>{
-                console.log(user);
-                if(user) {
-                    res.render("registration.ejs");
-                } else {
-                    const newUser = new User({
-                        name : name,
-                        login : login,
-                        password : password
-                    });
-                    newUser.save(function(err){
-                        if (err) {
-                            console.log(err);
-                            res.redirect("back");
-                        }
-                        else {
-                            passport.authenticate('local',{
-                                successRedirect : '/main',
-                                failureRedirect : '/login'
-                            })(req,res);
-                        }
-                    });
+
+    if (password.length < 6) {
+        return res.render("registration.ejs");
+    }
+    User.findOne({login : login}).exec((err,user)=>{
+        console.log(user);
+        if(user) {
+            return res.render("registration.ejs");
+        } else {
+            const newUser = new User({
+                name : name,
+                login : login,
+                password : password
+            });
+            newUser.save(function(err){
+                if (err) {
+                    console.log(err);
+                    return res.redirect("back");
+                }
+                else {
+                    passport.authenticate('local',{
+                        successRedirect : '/main',
+                        failureRedirect : '/login'
+                    })(req,res);
                 }
             });
         }
-    }
+    });
 });
 
 module.exports = router;

567d3e8d5104e2c48e018bdb809789796cb158b0
Evgeniy <maximov.evgen@gmail.com>
2020-08-01 18:35:12 +0300

    Решение вопроса #27

--- Data/Registration/scripts/validation.js
+++ Data/Registration/scripts/validation.js
@@ -1,30 +1,36 @@
 document.getElementById('password').addEventListener('keyup', function(e) {
     var value = this.value;
 	var result = false;
+	let pas = document.getElementById("error-password");
 	
 	if( value.match(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[!$%&*.-_])[0-9a-zA-Z!$%&*.-_]{8,16}$/) ) {
 		result = true;
 		document.getElementById('error-password').style.display="none";
+		pas.className = "error-pas-closed";
     }
 	else
 	{
         e.preventDefault();
 		document.getElementById('error-password').style.display="block";
+		pas.className = "error-pas-opened";
 	}
 });
 
 document.getElementById('login').addEventListener('keyup', function(e) {
     var value = this.value;
 	var result = false;
+	let log = document.getElementById("error-login");
 	
 	if( value.match(/^[a-zA-Z0-9_-]{3,16}$/) ) {
 		result = true;
 		document.getElementById('error-login').style.display="none";
+		log.className = "error-log-closed";
     }
 	else
 	{
         e.preventDefault();
 		document.getElementById('error-login').style.display="block";
+		log.className = "error-log-opened";
 	}
 
 });
@@ -32,15 +38,18 @@ document.getElementById('login').addEventListener('keyup', function(e) {
 document.getElementById('name').addEventListener('keyup', function(e) {
     var value = this.value;
 	var result = false;
+	let nam = document.getElementById("error-name");
 	
 	if( value.match(/^[a-zA-Z]{3,24}$/) ) {
 		result = true;
 		document.getElementById('error-name').style.display="none";
+		nam.className = "error-na-closed";
     }
 	else
 	{
         e.preventDefault();
 		document.getElementById('error-name').style.display="block";
+		nam.className = "error-na-opened";
 	}
  
 });

63de26002cd725b533481a72bff9d29f93fce27b
Nikita Rezepin <rezepinn@yandex.ru>
2020-08-04 20:46:06 +0300

    Изменения как в коммите #d018f21 применены к странице авторизации (а не к странице регистрации)

--- Data/Login/scripts/eye-opener.js
+++ Data/Login/scripts/eye-opener.js
@@ -2,12 +2,11 @@
 
 function togglePassword() {
     let password = document.getElementById("password");
-    let eye = document.getElementById("password-hider");
     if (password.type === "password") {
         password.type = "text";
-        eye.className = "eye-opened";
+        document.getElementById("password-closed").id = "password-opened";
     } else {
         password.type = "password";
-        eye.className = "eye-closed";
+        document.getElementById("password-opened").id = "password-closed";
     }
 }

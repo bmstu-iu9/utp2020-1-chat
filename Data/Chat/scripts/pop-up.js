318713e5db4dddc8732094c9284ab1bac07718ba
Evgeniy <maximov.evgen@gmail.com>
2020-08-09 06:48:30 +0300

    Выполнение вопроса #26

--- Data/Chat/scripts/pop-up.js
+++ Data/Chat/scripts/pop-up.js
@@ -6,11 +6,13 @@ function popUp() {
         button.id = "active-pop-up";
         document.getElementById("about").id = "active-about";
         document.getElementById("settings").id = "active-settings";
+		document.getElementById("leave").id = "active-leave";
         document.getElementById("logout").id = "active-logout";
     } else {
         document.getElementById("active-pop-up").id = "pop-up";
         document.getElementById("active-about").id = "about";
         document.getElementById("active-settings").id = "settings";
+		document.getElementById("active-leave").id = "leave";
         document.getElementById("active-logout").id = "logout";
     }
 }

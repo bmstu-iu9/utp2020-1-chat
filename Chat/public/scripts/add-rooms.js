fc28d3106a36ac0e8af0682571b9c723218e7ab9
Nikita Rezepin <rezepinn@yandex.ru>
2020-08-19 23:22:36 +0300

    Исправил отображение картинки внутри блока комнаты

--- Chat/public/scripts/add-rooms.js
+++ Chat/public/scripts/add-rooms.js
@@ -2,6 +2,7 @@
     let rooms = document.getElementById("rooms-list");
     let room = rooms.getElementsByClassName("room");
     for (let i = 0; i < room.length; i++) {
+        room[i] = room[i].getElementsByTagName("a")[0];
         let name = room[i].getElementsByClassName("room-name")[0].getElementsByTagName("a")[0].innerText;
         let roomImg = document.createElement("div");
         roomImg.className = "room-image";

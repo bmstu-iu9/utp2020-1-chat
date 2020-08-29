933f8c2809ef4c1371c9ccd1ad33454d09652175
MaksimKoster <m.kostritsya@yandex.ru>
2020-08-29 23:41:10 +0300

    Изменил реализацию эмодзи

--- Chat/public/scripts/add-emoji.js
+++ Chat/public/scripts/add-emoji.js
@@ -1,76 +1,28 @@
+let all_emojies = ["😀", "😃", "😄", "😁", "😆",
+"😅", "😂", "🤣", "😊", "😇", "🙂", "🙃", "😉", "😌", "😍",
+"🥰", "😘", "😗", "😙", "😚", "😋", "😛", "😝", "😜", "🤪",
+"🤨", "🧐", "🤓", "😎", "🤩", "🥳", "😏", "😒", "😞", "😔",
+"😟", "😕", "🙁", "☹", "😣", "😖", "😫", "😩", "🥺", "😢",
+"😭", "😤", "😠", "😡", "🤬", "🤯", "😳", "🥵", "🥶", "😱",
+"😨", "😰", "😥", "😓", "🤗", "🤔", "🤭", "🤫", "🤥", "😶",
+"😐", "😑", "😬", "🙄", "😯", "😦", "😧", "😮", "😲", "😴",
+"🤤", "😪", "😵", "🤐", "🥴", "🤢", "🤮", "🤧", "😷", "🤒",
+"🤕", "🤑", "🤠", "😈", "👿", "💩", "❤", "️💔", "🔝", "👍",
+"🤝", "👊🏻", "✊🏻", "🤛🏻", "🤜🏻", "💋"];
+
+const emoji_block = document.getElementById("emoji-block");
+
+for (let i = 0; i < all_emojies.length; i++){
+    emoji_block.innerHTML +=
+        '<button data-emoji value="' + all_emojies[i] + '" class="emoji-butt">' + all_emojies[i] + '</button>';
+}
+
 const message = document.getElementById("m");
 
-const emoji_1 = document.getElementById("😀");
-emoji_1.addEventListener("click", function () {
-    message.value += emoji_1.value;
-});
+const emojies = document.querySelectorAll('[data-emoji]');
 
-const emoji_2 = document.getElementById("😃");
-emoji_2.addEventListener("click", function () {
-    message.value += emoji_2.value;
-});
-
-const emoji_3 = document.getElementById("😄");
-emoji_3.addEventListener("click", function () {
-    message.value += emoji_3.value;
-});
-
-const emoji_4 = document.getElementById("😁");
-emoji_4.addEventListener("click", function () {
-    message.value += emoji_4.value;
-});
-
-const emoji_5 = document.getElementById("😆");
-emoji_5.addEventListener("click", function () {
-    message.value += emoji_5.value;
-});
-
-const emoji_6 = document.getElementById("😅");
-emoji_6.addEventListener("click", function () {
-    message.value += emoji_6.value;
-});
-
-const emoji_7 = document.getElementById("😂");
-emoji_7.addEventListener("click", function () {
-    message.value += emoji_7.value;
-});
-
-const emoji_8 = document.getElementById("🤣");
-emoji_8.addEventListener("click", function () {
-    message.value += emoji_8.value;
-});
-
-const emoji_9 = document.getElementById("😊");
-emoji_9.addEventListener("click", function () {
-    message.value += emoji_9.value;
-});
-
-const emoji_10 = document.getElementById("😇");
-emoji_10.addEventListener("click", function () {
-    message.value += emoji_10.value;
-});
-
-const emoji_11 = document.getElementById("🙂");
-emoji_11.addEventListener("click", function () {
-    message.value += emoji_11.value;
-});
-
-const emoji_12 = document.getElementById("🙃");
-emoji_12.addEventListener("click", function () {
-    message.value += emoji_12.value;
-});
-
-const emoji_13 = document.getElementById("😉");
-emoji_13.addEventListener("click", function () {
-    message.value += emoji_13.value;
-});
-
-const emoji_14 = document.getElementById("😌");
-emoji_14.addEventListener("click", function () {
-    message.value += emoji_14.value;
-});
-
-const emoji_15 = document.getElementById("😍");
-emoji_15.addEventListener("click", function () {
-    message.value += emoji_15.value;
-});
+emojies.forEach(button => {
+    button.addEventListener("click", () => {
+        message.value += button.value;
+    })
+})
